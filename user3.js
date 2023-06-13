aeafcfb3c9ff406e292efe8faf0a8d118c1db53a
Keerthi033 <93459235+Keerthi033@users.noreply.github.com>
2023-06-13 22:29:34 +0530

    addedjs file

--- user3.js
+++ user3.js
@@ -16,7 +16,7 @@ let getRandomUser = function(){
         displayobject.name = data.results[0].name.first + " " + data.results[0].name.last
         displayobject.imageurl = data.results[0].picture.large
         displayobject.description = data.results[0].gender        
-    document.getElementById("il").src=displayobject.imgurl;
+    document.getElementById("il").src=displayobject.imageurl;
     document.getElementById("n").innerHTML=displayobject.name;
     document.getElementById("d").innerHTML=displayobject.description;
     })
